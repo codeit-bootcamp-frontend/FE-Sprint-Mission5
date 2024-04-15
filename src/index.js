869055e72a63206e15647aac3259bdfdac472c6e
Kathleen Kang <kathleen@treasurer.co.kr>
2024-04-16 03:43:20 +0900

    feat: add global stylesheet

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/global.css"; // 전역 스타일시트
+import "./styles/global.css"; // index.js에서 global stylesheet을 import하면 전역적으로 스타일이 적용돼요
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
