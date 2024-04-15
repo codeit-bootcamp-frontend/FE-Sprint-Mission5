71b925eb6f669c411e15b53f8beb300c662fc453
Kathleen Kang <kathleen@treasurer.co.kr>
2024-04-16 03:42:54 +0900

    feat: install react-router-dom and set up initial routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import MarketPage from "./pages/MarketPage/MarketPage";
+import AddItemPage from "./pages/AddItemPage/AddItemPage";
+import CommunityFeedPage from "./pages/CommunityFeedPage/CommunityFeedPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="items" element={<MarketPage />} />
+        <Route path="additem" element={<AddItemPage />} />
+        <Route path="community" element={<CommunityFeedPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
