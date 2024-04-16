f693eda7e617ddce8ee6d19e68d741cfafa0fc56
Kathleen Kang <kathleen@treasurer.co.kr>
2024-04-16 12:46:54 +0900

    chore: limit item card name to a single line

--- src/pages/MarketPage/components/ItemCard.jsx
+++ src/pages/MarketPage/components/ItemCard.jsx
@@ -4,11 +4,7 @@ import { ReactComponent as HeartIcon } from "../../../assets/images/icons/ic_hea
 function ItemCard({ item }) {
   return (
     <div className="itemCard">
-      <img
-        src={item.images[0]}
-        alt={item.name}
-        className="itemCardThumbnail"
-      />
+      <img src={item.images[0]} alt={item.name} className="itemCardThumbnail" />
       <div className="itemSummary">
         <h2 className="itemName">{item.name}</h2>
         <p className="itemPrice">{item.price.toLocaleString()}원</p>
