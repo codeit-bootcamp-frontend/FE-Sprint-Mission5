d5a60b15fca3a0fb0e86934b45f66d07f82ebab4
Kathleen Kang <kathleen@treasurer.co.kr>
2024-04-16 10:22:05 +0900

    feat: add searchbar and button in market all items section

---
+++ src/components/UI/DropDownMenu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DropDownMenu() {
+  return (
+    <div className="dropdownMenu">
+      <div className="dropdownItem">최신순</div>
+      <div className="dropdownItem">인기순</div>
+    </div>
+  );
+}
+export default DropDownMenu;
--- src/pages/MarketPage/components/AllItemsSection.jsx
+++ src/pages/MarketPage/components/AllItemsSection.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { getProducts } from "../../../api/itemApi";
 import ItemCard from "./ItemCard";
+import { ReactComponent as SortIcon } from "../../../assets/images/icons/ic_sort.svg";
+import { ReactComponent as SearchIcon } from "../../../assets/images/icons/ic_search.svg";
+import { Link } from "react-router-dom";
+import DropDownMenu from "../../../components/UI/DropDownMenu";
 
 const getPageSize = () => {
   const width = window.innerWidth;
@@ -21,6 +25,7 @@ function AllItemsSection() {
   const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(getPageSize());
   const [itemList, setItemList] = useState([]);
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
 
   const fetchSortedData = async ({ orderBy, page, pageSize }) => {
     const products = await getProducts({ orderBy, page, pageSize });
@@ -42,9 +47,33 @@ function AllItemsSection() {
     };
   }, [orderBy, page, pageSize]);
 
+  const toggleDropdown = () => {
+    setIsDropdownVisible(!isDropdownVisible);
+  };
+
   return (
     <div>
-      <h1 className="sectionTitle">판매 중인 상품</h1>
+      <div className="allItemsSectionHeader">
+        <h1 className="sectionTitle">판매 중인 상품</h1>
+        <Link to="/additem" className="loginLink button">
+          상품 등록하기
+        </Link>
+      </div>
+
+      <div className="allItemsSectionHeader">
+        <div className="searchBarWrapper">
+          <SearchIcon />
+          <input
+            className="searchBarInput"
+            placeholder="검색할 상품을 입력해 주세요"
+          />
+        </div>
+        <button className="sortDropDownTriggerButton" onClick={toggleDropdown}>
+          <SortIcon />
+        </button>
+        {isDropdownVisible && <DropDownMenu />}
+      </div>
+
       <div className="allItemsCardSection">
         {itemList?.map((item) => (
           <ItemCard item={item} key={`market-item-${item.id}`} />
