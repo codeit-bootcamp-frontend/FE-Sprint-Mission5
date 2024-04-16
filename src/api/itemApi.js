82ab9a27652b7b69b1f47876dd10702c8771f9de
Kathleen Kang <kathleen@treasurer.co.kr>
2024-04-16 12:51:42 +0900

    chore: add comment

--- src/api/itemApi.js
+++ src/api/itemApi.js
@@ -1,4 +1,5 @@
 export async function getProducts(params = {}) {
+  // 자바스크립트의 `URLSearchParams`을 이용하면 파라미터 값을 자동으로 쉽게 인코딩할 수 있어요.
   const query = new URLSearchParams(params).toString();
   const response = await fetch(
     `https://panda-market-api.vercel.app/products?${query}`
